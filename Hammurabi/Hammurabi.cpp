ca50258ab039d31573b8b15b0008243469c79b42
KACHANIX <kochaser@gmail.com>
2021-10-07 22:49:52 +0300

    [add] game final, [must-add] savefile

--- Hammurabi/Hammurabi.cpp
+++ Hammurabi/Hammurabi.cpp
@@ -49,7 +49,18 @@ private:
 	int acres_to_sell_;
 	int bushels_to_eat_;
 	int acres_to_sow_;
+	int harvested_acres_;
+	int harvest_from_acre;
+
+	double yearly_starved_;
+	void GeneratePrice()
+	{
+		std::mt19937 gen(time(0));
+		std::uniform_int_distribution<> uid(17, 26);
+		price_ = uid(gen);
+	}
 public:
+
 	Babylon()
 	{
 		population_ = 100;
@@ -58,29 +69,123 @@ public:
 		year_ = 1;
 
 		harvest_ = 0;
+		harvested_acres_ = 1;
 		starved_people_ = 0;
 		new_people_ = 0;
 		bushels_destroyed_ = 0;
+		harvest_from_acre = 0;
 		plague_ = false;
+		yearly_starved_ = 0;
 		GeneratePrice();
 	}
 
-	void GeneratePrice()
-	{
-		std::mt19937 gen(time(0));
-		std::uniform_int_distribution<> uid(17, 26);
-		price_ = uid(gen);
+	bool CheckLoss() {
+		double a = (double)starved_people_ / (starved_people_ + (plague_ ? 2 : 1) * population_ - new_people_);
+		yearly_starved_ += a;
+		if (population_ == 0 || a >= 0.45)
+		{
+			std::cout << "Вы уничтожили город. До свидания!";
+			return true;
+		}
+		return false;
 	}
 
-	int CountStarved()
+	bool CheckLastYear()
 	{
-		return 1;
-
+		if (year_ == 11)
+		{
+			yearly_starved_ /= 10;
+			int acres_per_human = population_ / acres_;
+			if (yearly_starved_ > 0.33 && acres_per_human < 7)
+			{
+				std::cout << "\nИз-за вашей некомпетентности в управлении, народ устроил бунт, и изгнал вас их города.Теперь вы вынуждены влачить жалкое существование в изгнании";
+			}
+			else if (yearly_starved_ > 0.1 && acres_per_human < 9)
+			{
+				std::cout << "\nВы правили железной рукой, подобно Нерону и Ивану Грозному. Народ вздохнул с облегчением, и никто больше не желает видеть вас правителем";
+			}
+			else if (yearly_starved_ > 0.03 && acres_per_human < 10)
+			{
+				std::cout << "\nВы справились вполне неплохо, у вас, конечно, есть недоброжелатели, но многие хотели бы увидеть вас во главе города снова";
+			}
+			else
+			{
+				std::cout << "\nФантастика! Карл Великий, Дизраэли и Джефферсон вместе не справились бы лучше";
+			}
+			return true;
+		}
+		return false;
 	}
 
 	void UpdateCityState()
 	{
 
+		// bushels volume after king's orders
+		bushels_ = bushels_ + (acres_to_sell_ - acres_to_buy_) * price_ - bushels_to_eat_ - acres_to_sow_ * 0.5;
+
+		// new acreage
+		acres_ = acres_ + acres_to_buy_ - acres_to_sell_;
+
+		// starved people
+		if (bushels_to_eat_ / 20 < population_)
+		{
+			starved_people_ = population_ - bushels_to_eat_ / 20;
+			population_ -= starved_people_;
+		}
+
+		// harvest by alive people
+		std::mt19937 gen(time(0));
+		std::uniform_int_distribution<> hid(1, 6);
+		harvest_from_acre = hid(gen);
+		int max_harvested_acres = (population_ - starved_people_) * 10;
+		if (max_harvested_acres > acres_to_sow_)
+		{
+			harvest_ = acres_to_sow_ * harvest_from_acre;
+			harvested_acres_ = acres_to_sow_;
+		}
+		else
+		{
+			harvest_ = max_harvested_acres * harvest_from_acre;
+			harvested_acres_ = max_harvested_acres;
+		}
+		bushels_ += harvest_;
+
+		// rats
+		std::uniform_real_distribution<> rid(0.0, 0.07);
+		bushels_destroyed_ = rid(gen) * bushels_;
+		bushels_ -= bushels_destroyed_;
+
+		// plague
+		std::uniform_int_distribution<> pid(1, 100);
+		if (pid(gen) <= 15)
+		{
+			plague_ = true;
+			population_ /= 2;
+		}
+		else
+		{
+			plague_ = false;
+		}
+
+		// newcomers
+		int newcomers = starved_people_ / 2 + (5 - harvest_from_acre) * bushels_ / 600 + 1;
+		if (newcomers <= 0)
+		{
+			new_people_ = 0;
+		}
+		else if (newcomers >= 50)
+		{
+			new_people_ = 50;
+		}
+		else
+		{
+			new_people_ = newcomers;
+		}
+		population_ += new_people_;
+
+		// next year 
+		GeneratePrice();
+		year_++;
 	}
 
 	void PrintYearReport()
@@ -104,7 +209,7 @@ public:
 			std::cout << "\n\t" << "Чума обошла наш город стороной;";
 		}
 		std::cout << "\n\tНаселение города составляет " << population_ << " человек;";
-		std::cout << "\n\tМы собрали " << harvest_ << " бушелей пшеницы, по " << harvest_ / acres_ << " бушеля с акра;";
+		std::cout << "\n\tМы собрали " << harvest_ << " бушелей пшеницы, по " << harvest_from_acre << " бушеля с акра;";
 		std::cout << "\n\tКрысы истребили " << bushels_destroyed_ << " бушелей пшеницы, оставив " << bushels_ << " бушеля в амбарах; ";
 		std::cout << "\n\tГород сейчас занимает " << acres_ << " акров;";
 		std::cout << "\n\tЦена одного акра составляет " << price_ << " бушелей.";
@@ -126,7 +231,7 @@ public:
 					std::cout << "Повелитель, это невозможно. Повтори, пожалуйста: ";
 					continue;
 				}
-				if (acres_to_buy_ * price_ > bushels_)
+				if (acres_to_buy_ != 0 && acres_to_buy_ * price_ > bushels_)
 				{
 					std::cout << "Повелитель, мы не можем себе этого позволить. Повтори, пожалуйста:";
 					continue;
@@ -145,7 +250,7 @@ public:
 					std::cout << "Повелитель, это невозможно. Повтори, пожалуйста: ";
 					continue;
 				}
-				if (acres_to_sell_ > acres_to_buy_ + acres_)
+				if (acres_to_sell_ != 0 && acres_to_sell_ > acres_to_buy_ + acres_)
 				{
 					std::cout << "Повелитель, мы не можем себе этого позволить с учетом предыдущих действий. Повтори, пожалуйста:";
 					continue;
@@ -164,7 +269,7 @@ public:
 					std::cout << "Повелитель, это невозможно. Повтори, пожалуйста: ";
 					continue;
 				}
-				if (bushels_to_eat_ > bushels_ + (acres_to_sell_ - acres_to_buy_) * price_)
+				if (bushels_to_eat_ != 0 && bushels_to_eat_ > bushels_ + (acres_to_sell_ - acres_to_buy_) * price_)
 				{
 					std::cout << "Повелитель, мы не можем себе этого позволить с учетом предыдущих действий. Повтори, пожалуйста:";
 					continue;
@@ -173,7 +278,7 @@ public:
 			}
 
 			// bushels to sow
-			std::cout << "\nСколько бушелей повелеваешь отдать на посев? ";
+			std::cout << "\nСколько акров земли повелеваешь засеять? ";
 			acres_to_sow_ = 0;
 			while (true)
 			{
@@ -183,8 +288,8 @@ public:
 					std::cout << "Повелитель, это невозможно. Повтори, пожалуйста: ";
 					continue;
 				}
-				if (acres_to_sow_ > acres_ + acres_to_buy_ - acres_to_sell_ ||
-					acres_to_sow_ * 0.5 > (acres_to_sell_ - acres_to_buy_) * price_ + bushels_ - bushels_to_eat_)
+				if (acres_to_sow_ != 0 && (acres_to_sow_ > acres_ + acres_to_buy_ - acres_to_sell_ ||
+					acres_to_sow_ * 0.5 > (acres_to_sell_ - acres_to_buy_) * price_ + bushels_ - bushels_to_eat_))
 				{
 					std::cout << "Повелитель, мы не можем себе этого позволить с учетом предыдущих действий. Повтори, пожалуйста:";
 					continue;
@@ -193,21 +298,20 @@ public:
 			}
 
 			//confirm the order
-			std::cout << "Повелитель, приказ сформирован, подпишешь ли ты его? Если нет, то создадим новый. (yes/no)";
+			std::cout << "Повелитель, приказ сформирован, подпишешь ли ты его? Если нет, то создадим новый. (y/n)";
 			while (true)
 			{
 				std::cin >> input_string;
-				if (input_string == "yes" || input_string == "no")
+				if (input_string == "y" || input_string == "n")
 				{
 					break;
 				}
 				std::cout << "Повелитель, это невозможно. Повтори, пожалуйста: ";
 			}
 
-			if (input_string == "yes")
+			if (input_string == "y")
 				break;
 		}
-
 	}
 };
 
@@ -225,21 +329,15 @@ int main()
 	while (true)
 	{
 		babylon.PrintYearReport();
+		if (babylon.CheckLastYear())
+		{
+			break;
+		}
 		babylon.GetKingsOrders();
+		babylon.UpdateCityState();
+		if (babylon.CheckLoss())
+		{
+			break;
+		}
 	}
-
-
-	std::cout << "Здарова!\n";
-
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
